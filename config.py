f387bce6a3b9a76c9bf2a28cc7ec3dce656ee3d7
Divesh-M <diveshkumar930@gmail.com>
2024-12-29 18:25:53 +0530

    t p
    exit
    exit()
    quit
    
    i#

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 # Configuration file for constants
 
 # Replace with your actual OpenAI API key
-OPENAI_API_KEY = "sk-None-ZtjV0Z4obUM3Xd2uau2KT3BlbkFJCZSs1bhI9GmQjDqORl2J"
+OPENAI_API_KEY = "YOUR_OPEN_API_KEY"
