6c7f0c96a7df4542a264f43ad92200b88f54f88c
Divesh-M <diveshkumar930@gmail.com>
2024-12-30 15:36:04 +0530

    test

--- test.py
+++ test.py
@@ -4,6 +4,8 @@ def fibonacci(n):
     for i in range(2, n):
         next_term = fib_series[-1] + fib_series[-2]
         fib_series.append(next_term)
+
+    print(fib_series)
     
     return fib_series
 
